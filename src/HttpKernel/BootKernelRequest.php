a54c88a459eed3d81c00b8d3eaa3bb138a1773a5
Wenzel Jonas <wenzel.jonas@huckleberry-friends.de>
2017-06-19 16:51:20 +0200

    Added missing copyright comment in src/HttpKernel/BootKernelRequest.php

--- src/HttpKernel/BootKernelRequest.php
+++ src/HttpKernel/BootKernelRequest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Motana package.
+ *
+ * (c) Wenzel Jonas <mail@ramihyn.sytes.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Motana\Bundle\MultikernelBundle\HttpKernel;
 
 use Symfony\Component\HttpFoundation\Request;
